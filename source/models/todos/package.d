b6d971adccecd42e0757555e7777d4c86a7c0214
UIM (Sicherheitsschmiede) <ui.manufaktur@gmail.com>
2022-05-12 11:46:56 +0200

    Import attributes

--- source/models/todos/package.d
+++ source/models/todos/package.d
@@ -6,9 +6,10 @@ public import std.uuid;
 
 public import uim.core;
 public import uim.oop;
-public import uim.models;
+public import uim.entities;
 
 // Packages
+public import models.todos.attributes;
 public import models.todos.classes;
 public import models.todos.entities;
 public import models.todos.tests;
